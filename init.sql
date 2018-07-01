60c48bf26e190fc82e1dc8fb70c37e44f761052e
Freddy Fallon <freddyfallon@gmail.com>
2018-07-01 21:29:05 +0100

    🔧 Update init.sql to add some items into stock

--- init.sql
+++ init.sql
@@ -1,1 +1,2 @@
-CREATE TABLE stock(id SERIAL PRIMARY KEY, item VARCHAR(40) not null, quantity INTEGER)
+CREATE TABLE stock(id SERIAL PRIMARY KEY, item VARCHAR(40) not null, quantity INTEGER);
+INSERT INTO stock(item, quantity) VALUES ("ham", 3)
